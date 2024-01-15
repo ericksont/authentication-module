28e42ab8c31ecee7a0ff3a7795fd301cca61a949
Erickson Tavares <erickson@ericksontavares.com.br>
2024-01-14 23:09:59 -0300

    clean project

--- www/authentication/index.php
+++ www/authentication/index.php
@@ -1,50 +1,44 @@
 <?php $ENVIRONMENT = parse_ini_file('/var/www/Library/Authentication/.dev.env'); ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Tela de Login</title>
 
-    <link rel="stylesheet" href="css/style.css">
-    
-</head>
-<body>
-    <div class="root">
+    <?php require_once 'requires/head.php'; ?>
 
-        <div class="banners" ></div>
+    <body>
+        <div class="root">
 
-        <div class="content">
+            <div class="banners" ></div>
 
-            <section class="animate container login-form">
-                <h1> Autenticação </h1>
-                <form>
-                    <input type="text" id="user" placeholder="E-mail" />
-                    <input type="password" id="pass" placeholder="Senha" />
-                    <div class="actions">
-                        <a class="btn" href="javascript:;" onClick="authentication.login()">Entrar</a>
+            <div class="content">
+
+                <section class="animate container login-form">
+                    <h1> Autenticação </h1>
+                    <form>
+                        <input type="text" id="user" placeholder="E-mail" />
+                        <input type="password" id="pass" placeholder="Senha" />
+                        <div class="actions">
+                            <a class="btn" href="javascript:;" onClick="authentication.login()">Entrar</a>
+                        </div>
+                    </form>
+                    
+                    <div class="separator">
+                        <h1>d'Sousa</h1>                    
                     </div>
-                </form>
-                
-                <div class="separator">
-                    <h1>d'Sousa</h1>                    
-                </div>
-                
-            </section>
-          
-            <div class="loading"></div>
+                    
+                </section>
             
+                <div class="loading"></div>
+                
+            </div>
+
         </div>
 
-    </div>
+        <script src="js/authentication.js"></script>
 
-    <script src="js/authentication.js"></script>
-
-    <script>
-        const API = '<?php print $ENVIRONMENT['API'] ?>'
-    </script>
-    
-</body>
+        <script>
+            document.title = document.title + ' | Autenticação '
+            const API = '<?php print $ENVIRONMENT['API'] ?>'
+        </script>
+        
+    </body>
 </html>
